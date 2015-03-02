b715cf964ae20a8fcf348510325081c8bcc19381
Alex Yakubovich <alex.yakubovich@gmail.com>
2015-03-02 11:57:10 -0500

    create home doc file for testr
    ?testr will lead here

--- R/testr-package.r
+++ R/testr-package.r
@@ -1,5 +1,6 @@
-#' testr.
+#' Bayesian A/B Testing Engine
 #'
+#'  Tools for Bayesian design and analysis of of randomized experiments (A/B/../n tests). testr uses an adaptive design (i.e. early stopping rules), allowing tests to end as early as possible.
 #' @name testr
 #' @docType package
 NULL
